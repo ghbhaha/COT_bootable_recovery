b16b746fa37a68c699cbffe31206fc0b9db2d2ef
IngCr3at1on <sblood86@gmail.com>
2012-06-24 02:01:59 -0500

    Revert "Implemented advanced backup, being able to choose what partitions to backup"
    This reverts commit 1fb6391aeaf38694fdbfcd51ddde4023c85d5093.
    
    Can't restore advanced backups...

--- nandroid.h
+++ nandroid.h
@@ -3,8 +3,9 @@
 
 int nandroid_main(int argc, char** argv);
 int nandroid_backup(const char* backup_path);
-int nandroid_advanced_backup(const char* backup_path, int boot, int recovery, int system, int data, int cache, int sdext, int wimax);
 int nandroid_restore(const char* backup_path, int restore_boot, int restore_system, int restore_data, int restore_cache, int restore_sdext, int restore_wimax);
 
+// Add this to the header so that extended commands can make use of it.
 void ensure_directory(const char* dir);
+
 #endif
