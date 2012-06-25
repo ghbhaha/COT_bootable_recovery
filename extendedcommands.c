ec1fc605cb2db08649e0d724e6a7c6d12318c2cb
Drew Walton <drewwalton19216801@gmail.com>
2012-06-24 18:24:08 -0600

    Notify the user that we are indeed deleting the backup

--- extendedcommands.c
+++ extendedcommands.c
@@ -407,6 +407,7 @@ void delete_old_backups(const char *mount_point)
 	sprintf(confirm, "Yes - Delete %s", basename(file));
 	if(confirm_selection(confirm_delete, confirm)) {
 		static char* tmp[PATH_MAX];
+        ui_print("Deleting backup...\n");
 		sprintf(tmp, "rm -rf %s", file);
 		__system(tmp);
 	}
