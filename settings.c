a512d65182b00d0f79d82959bd2a51a939e473e6
Drew Walton <drewwalton19216801@gmail.com>
2012-07-13 13:23:00 -0600

    Add initial settings menu
    Move theme settings to new settings menu

--- settings.c
+++ settings.c
@@ -56,3 +56,65 @@
 #include "mtdutils/mtdutils.h"
 #include "bmlutils/bmlutils.h"
 #include "colorific.h"
+
+#include "settings.h"
+#include "settingshandler.h"
+
+void show_settings_menu() {
+    static char* headers[] = { "COT Settings",
+                                "",
+                                NULL
+    };
+
+    static char* list[] = { "Theme",
+                            "ORS Forced Reboots",
+                            "ORS Nandroid Prompt",
+                            "ZIP flash Nandroid Prompt",
+                            NULL
+    };
+
+    for (;;) {
+        int chosen_item = get_menu_selection(headers, list, 0, 0);
+        switch (chosen_item) {
+            case GO_BACK:
+                return;
+            case 0:
+                show_theme_menu();
+                break;
+            case 1:
+                //show_ors_reboot_menu();
+                break;
+            case 2:
+                //show_ors_nandroid_prompt_menu();
+                break;
+            case 3:
+                //show_nandroid_prompt_menu();
+                break;
+            default:
+                return;
+        }
+    }
+}
+
+void show_theme_menu() {
+    static char* theme_headers[] = {"Theme", "", NULL};
+
+    static char* theme_list[] = { "Hydro (default)",
+                                  "Blood Red",
+                                  "Key Lime Pie",
+                                  "Citrus Orange",
+                                  "Dooderbutt Blue",
+                                  NULL
+    };
+
+    for (;;) {
+        int theme_sel = get_menu_selection(theme_headers, theme_list, 0, 0);
+        if(theme_sel = GO_BACK)
+            return;
+        else {
+            set_ui_color(theme_sel);
+            ui_reset_icons();
+            break;
+        }
+    }
+}
