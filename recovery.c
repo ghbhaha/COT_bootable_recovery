dcf03e4f26f895275f538268be980f2408153c05
Drew Walton <drewwalton19216801@gmail.com>
2012-08-02 14:50:17 -0600

    Add Utilities menu, move usb mount and sd partition to new menu

--- recovery.c
+++ recovery.c
@@ -45,6 +45,7 @@
 #include "settingshandler.h"
 #include "settingshandler_lang.h"
 #include "power.h"
+#include "utilities.h"
 
 #include "extendedcommands.h"
 #include "flashutils/flashutils.h"
@@ -792,6 +793,9 @@ prompt_and_wait() {
             case ITEM_COTOPTIONS:
                 show_cot_options_menu();
                 break;
+			case ITEM_UTILITIES:
+				show_utilities_menu();
+				break;
             case ITEM_POWEROPTIONS:
                 //poweroff=1;
 				show_power_options_menu();
