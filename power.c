e7be4a16072bb30844b1cd43efaa360cffdecfd7
IngCr3at1on <sblood86@gmail.com>
2012-08-31 15:43:29 -0500

    Add in secondary options for custom_power profiles (used by kindle fire and ORS).

--- power.c
+++ power.c
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -41,7 +41,6 @@
 #include "cutils/properties.h"
 #include "firmware.h"
 #include "install.h"
-#include "make_ext4fs.h"
 #include "minui/minui.h"
 #include "minzip/DirUtil.h"
 #include "roots.h"
@@ -83,8 +82,23 @@ void show_power_options_menu() {
 				android_reboot(ANDROID_RB_RESTART2, 0, "recovery");
 				break;
 			case POWER_OPTIONS_ITEM_POWEROFF:
-				__system("reboot -p");
+				pass_shutdown_cmd();
 				break;
 		}
 	}
+}
+
+/* On the off chance that your device requires a non-standard command
+ * to reboot or power off from recovery simply replace that command in
+ * one of the following.
+ *
+ * It is highly unlikely you will need to change this for your device
+ * this was added for the soul purpose of patching reboot in the Amazon
+ * Kindle Fire which uses a binary command to reset the idme bootmode. */
+void pass_normal_reboot() {
+	android_reboot(ANDROID_RB_RESTART, 0, 0);
+}
+
+void pass_shutdown_cmd() {
+	android_reboot(ANDROID_RB_POWEROFF, 0, 0);
 }
