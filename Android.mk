eac9c5390cbe64a11219adfa8be13ac000733ff6
Drew Walton <drewwalton19216801@gmail.com>
2012-09-06 16:57:44 -0600

    Use resolution-specific resource sets
    Also, the "res" folder has been removed to to externalizing in the repos

--- Android.mk
+++ Android.mk
@@ -181,6 +181,7 @@ include $(commands_recovery_local_path)/flashutils/Android.mk
 include $(commands_recovery_local_path)/libcrecovery/Android.mk
 include $(commands_recovery_local_path)/minui/Android.mk
 include $(commands_recovery_local_path)/minelf/Android.mk
+include $(commands_recovery_local_path)/gui/Android.mk
 include $(commands_recovery_local_path)/minzip/Android.mk
 include $(commands_recovery_local_path)/mtdutils/Android.mk
 include $(commands_recovery_local_path)/mmcutils/Android.mk
