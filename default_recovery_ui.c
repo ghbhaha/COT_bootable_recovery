269dec5a3fe104bfb465d3c91b3bffbba904bbdb
Drew Walton <drewwalton19216801@gmail.com>
2012-09-07 11:32:52 -0600

    Final fix-ups, version bump to v2.1

--- default_recovery_ui.c
+++ default_recovery_ui.c
@@ -83,7 +83,7 @@ int TIME_POS=RIGHT_ALIGN;
 /* define the battery capacity sys interface file
  * don't change unless your device needs it!
  */
-char batt_cap = "/sys/class/power_supply/battery/capacity";
+char* batt_cap = "/sys/class/power_supply/battery/capacity";
 
 char* MENU_HEADERS[] = { NULL };
 
