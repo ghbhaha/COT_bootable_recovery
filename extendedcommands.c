a18c997f463f711700d51863b24f94d54345e33a
IngCr3at1on <sblood86@gmail.com>
2013-08-22 16:27:57 -0500

    Allow for custom lun file path.

--- extendedcommands.c
+++ extendedcommands.c
@@ -682,6 +682,9 @@ int control_usb_storage_for_lun(Volume* vol, bool enable) {
 #ifdef BOARD_UMS_LUNFILE
         BOARD_UMS_LUNFILE,
 #endif
+#ifdef TARGET_USE_CUSTOM_LUN_FILE_PATH
+        TARGET_USE_CUSTOM_LUN_FILE_PATH,
+#endif
         "/sys/devices/platform/usb_mass_storage/lun%d/file",
         "/sys/class/android_usb/android0/f_mass_storage/lun/file",
         "/sys/class/android_usb/android0/f_mass_storage/lun_ex/file",
